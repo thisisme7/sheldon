e05d1ac8caa47b89cec3a9e600bcd9c3af7dcd57
thisisme7 <daniel.monte710@gmail.com>
2024-11-06 13:09:51 +0000

    CSFR protection, redesign

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 import sheldon
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
+app.secret_key = b"9349t3n3doinrgn0heshn0eh"
+csrf = CSRFProtect(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -12,4 +15,4 @@ def main():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8002, debug=True)
+    app.run(host="0.0.0.0", port=8002, debug=True)
